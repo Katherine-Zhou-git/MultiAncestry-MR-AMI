bb9e0144710f395eab51a36c5d0208e53a6761bc
Katherine-Zhou-git <kathy.zhou78@gmail.com>
2026-06-27 05:23:32 -0400

    SAS_ForestPlot.R

--- Scripts/SAS_ForestPlot.R
+++ Scripts/SAS_ForestPlot.R
@@ -3,14 +3,14 @@ library(dplyr)
 library(patchwork)
 
 data_sa <- data.frame(
-  Phenotype = factor(rep(c("COVID-19 susceptibility", "Hospitalized COVID-19", "Very Severe COVID-19"), each = 5),
-                     levels = c("COVID-19 susceptibility", "Hospitalized COVID-19", "Very Severe COVID-19")),
+  Phenotype = factor(rep(c("COVID-19 Infection", "Hospitalized COVID-19", "Very Severe COVID-19"), each = 5),
+                     levels = c("COVID-19 Infection", "Hospitalized COVID-19", "Very Severe COVID-19")),
   
   Method = factor(rep(c("MR Egger", "Weighted Median", "IVW Fixed Effects", "IVW Random Effects", "Weighted Mode"), 3),
                   levels = rev(c("IVW Fixed Effects", "IVW Random Effects", "Weighted Median", "Weighted Mode", "MR Egger"))),
   
   OR = c(
-    # COVID-19 susceptibility (C2)
+    # COVID-19 Infection (C2)
     0.9447, 0.9270, 0.9278, 0.9278, 0.9450,
     # Hospitalized COVID-19 (B2)
     0.9274, 0.9176, 0.9170, 0.9170, 1.0241,
@@ -19,7 +19,7 @@ data_sa <- data.frame(
   ),
   
   CI_lower = c(
-    # COVID-19 susceptibility (C2)
+    # COVID-19 Infection (C2)
     0.4481, 0.6201, 0.6804, 0.7461, 0.3985,
     # Hospitalized COVID-19 (B2)
     0.6941, 0.7916, 0.8207, 0.8510, 0.7665,
@@ -28,7 +28,7 @@ data_sa <- data.frame(
   ),
   
   CI_upper = c(
-    # COVID-19 susceptibility (C2)
+    # COVID-19 Infection (C2)
     1.9918, 1.3858, 1.2652, 1.1539, 2.2408,
     # Hospitalized COVID-19 (B2)
     1.2390, 1.0637, 1.0245, 0.9881, 1.3683,
@@ -37,7 +37,7 @@ data_sa <- data.frame(
   ),
   
   p_val = c(
-    # COVID-19 susceptibility (C2)
+    # COVID-19 Infection (C2)
     0.883, 0.712, 0.636, 0.501, 0.900,
     # Hospitalized COVID-19 (B2)
     0.617, 0.254, 0.126, 0.023, 0.874,
@@ -46,7 +46,7 @@ data_sa <- data.frame(
   ),
   
   SNPs = c(
-    rep(15, 5),  # COVID-19 susceptibility
+    rep(15, 5),  # COVID-19 Infection
     rep(18, 5),  # Hospitalized COVID-19
     rep(19, 5)   # Very Severe COVID-19
   )
@@ -99,7 +99,7 @@ p_forest <- ggplot(table_data, aes(x = OR, y = Method)) +
   geom_errorbarh(aes(xmin = CI_lower, xmax = CI_upper, color = Phenotype), height = 0.2, linewidth = 0.8) +
   geom_point(aes(color = Phenotype), size = 2.5) +
   facet_grid(Phenotype ~ ., scales = "free_y", space = "free_y") +
-  scale_color_manual(values = c("COVID-19 susceptibility" = 'darkorchid1',
+  scale_color_manual(values = c("COVID-19 Infection" = 'darkorchid1',
                                 "Hospitalized COVID-19" = 'darkorchid1',
                                 "Very Severe COVID-19" = "darkorchid1")) +
   scale_x_continuous(position = "bottom", breaks = seq(0.5, 2.0, by = 0.5)) +
