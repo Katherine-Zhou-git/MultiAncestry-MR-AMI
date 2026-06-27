8af55ab54b4fac54fabba452600752aa97f3fd44
Katherine-Zhou-git <kathy.zhou78@gmail.com>
2026-06-27 02:56:28 -0400

    EUR_B2.R

--- Scripts/EUR_B2.R
+++ Scripts/EUR_B2.R
@@ -1,10 +1,9 @@
 # ================================================
-# EUR - Very Severe COVID-19 (B2) vs AMI
+# EUR - Hospitalized COVID-19 (B2) vs AMI
 # ================================================
 
 library(data.table)
 library(MendelianRandomization)
-library(MRPracticals)
 library(TwoSampleMR)
 library(LDlinkR)
 library(dplyr)
@@ -14,38 +13,40 @@ library(ieugwasr)
 library(genetics.binaRies)
 library(kableExtra)
 library(gt)
-library(phenoscanner)
 library(R.utils)
+library(MRPRESSO)
 
 rm(list = ls(all=TRUE))
 set.seed(123456)
 setwd("./COVID_GWAS")
 
-# Exposure
+# Exposure: Hospitalized COVID-19 (B2) - EUR
 exposure_raw <- fread("./COVID-19_AMI_All/COVID19_HGI_B2_ALL_eur_leave23andme_20220403.tsv.gz")
 exposure_c1 <- exposure_raw[, c(1,2,3,4,5,6,7,8,9,14,15)]
 colnames(exposure_c1) <- c("CHR","POS","other_allele.exposure","effect_allele.exposure",
-                          "SNP","N","beta.exposure","se.exposure","pval","eaf.exposure","rsid")
-exposure_c1$exposure <- "Severe_COVID19_B2"
+                           "SNP","N","beta.exposure","se.exposure","pval","eaf.exposure","rsid")
+
+exposure_c1$exposure <- "Hospitalized_COVID19_B2_EUR"
 exposure_c1$id.exposure <- "covid_b2_eur"
 exposure_c1$Z.exposure <- exposure_c1$beta.exposure / exposure_c1$se.exposure
 exposure_c1 <- filter(exposure_c1, pval < 5e-8)
 exposure_c1$F.exposure <- (exposure_c1$Z.exposure)^2
 
 exposure_snps <- ld_clump(dat = exposure_c1, clump_kb = 10000, clump_r2 = 0.001,
-                          plink_bin = genetics.binaRies::get_plink_binary(), bfile = "./EUR")
+                          plink_bin = genetics.binaRies::get_plink_binary(), bfile = "./1kg.v3/EUR")
+
+exposure_snps <- filter(exposure_snps, F.exposure > 10)
 
 # Outcome
-outcome_raw <- fread("./COVID-19_AMI_All/GCST90473536.h.tsv.gz")
+outcome_raw <- fread("./GCST90473536.h.tsv.gz")
 outcome_ss1 <- outcome_raw[, c(1,2,3,4,5,6,7,8,9)]
 colnames(outcome_ss1) <- c("CHR","POS","effect_allele.outcome","other_allele.outcome",
-                          "beta.outcome","se.outcome","eaf.outcome","pval","rsid")
+                           "beta.outcome","se.outcome","eaf.outcome","pval","rsid")
 outcome_ss1$outcome <- "AMI"
 outcome_ss1$id.outcome <- "ami_eur"
 outcome_ss1$Z.outcome <- outcome_ss1$beta.outcome / outcome_ss1$se.outcome
 outcome_clumped <- outcome_ss1
 
-# Harmonise
 mr_dat <- harmonise_data(
   dplyr::tibble(SNP = exposure_snps$rsid, beta.exposure = exposure_snps$beta.exposure,
                 se.exposure = exposure_snps$se.exposure, effect_allele.exposure = exposure_snps$effect_allele.exposure,
@@ -61,7 +62,7 @@ mr_dat <- harmonise_data(
 mr_dat$outcome[mr_dat$outcome == "ami"] <- "AMI"
 mr_dat <- mr_dat[mr_dat$mr_keep == TRUE & mr_dat$palindromic != TRUE, ]
 
-# MR-PRESSO
+# ==================== MR-PRESSO ====================
 results <- mr_presso(BetaOutcome = "beta.outcome", BetaExposure = "beta.exposure",
                      SdOutcome = "se.outcome", SdExposure = "se.exposure",
                      OUTLIERtest = TRUE, DISTORTIONtest = TRUE, data = mr_dat,
@@ -69,20 +70,35 @@ results <- mr_presso(BetaOutcome = "beta.outcome", BetaExposure = "beta.exposure
 
 cat("Global test p =", results$`MR-PRESSO results`$`Global Test`$Pvalue, "\n")
 
-# MR Analysis
-res <- mr(mr_dat, method_list = c("mr_egger_regression", "mr_weighted_median",
-                                  "mr_ivw_fe", "mr_ivw_mre", "mr_weighted_mode"))
+# ==================== Main MR Analysis ====================
+res <- mr(mr_dat, method_list = c(
+  "mr_egger_regression",
+  "mr_weighted_median",
+  "mr_ivw_fe",
+  "mr_ivw_mre",
+  "mr_weighted_mode"
+))
 
+# Save results
 odd <- generate_odds_ratios(res)
-write.csv(odd, "results/tables/EUR_B2_results.csv", row.names = FALSE)
+write.csv(odd, "./EUR_B2_results.csv", row.names = FALSE)
 
-# Plots
-ggsave("results/figures/EUR_B2_scatter.png", mr_scatter_plot(res, mr_dat)[[1]], width=8, height=6)
-ggsave("results/figures/EUR_B2_forest.png", mr_forest_plot(mr_singlesnp(mr_dat))[[1]], width=10, height=8)
-ggsave("results/figures/EUR_B2_leaveoneout.png", mr_leaveoneout_plot(mr_leaveoneout(mr_dat))[[1]], width=10, height=8)
-ggsave("results/figures/EUR_B2_funnel.png", mr_funnel_plot(mr_singlesnp(mr_dat))[[1]], width=8, height=6)
+# ==================== Plots ====================
+singlesnp_res <- mr_singlesnp(mr_dat, all_method = c(
+  "mr_egger_regression",
+  "mr_weighted_median",
+  "mr_ivw_fe",
+  "mr_ivw_mre",
+  "mr_weighted_mode"
+))
 
+ggsave("./EUR_B2_scatter.png", mr_scatter_plot(res, mr_dat)[[1]], width = 8, height = 6)
+ggsave("./EUR_B2_forest.png", mr_forest_plot(singlesnp_res)[[1]], width = 10, height = 8)
+ggsave("./EUR_B2_leaveoneout.png", mr_leaveoneout_plot(mr_leaveoneout(mr_dat))[[1]], width = 10, height = 8)
+ggsave("./EUR_B2_funnel.png", mr_funnel_plot(singlesnp_res)[[1]], width = 8, height = 6)
+
+# ==================== Sensitivity Tests ====================
 mr_heterogeneity(mr_dat)
 mr_pleiotropy_test(mr_dat)
 
-cat("EUR B2 Completed\n")
+cat("EUR B2 Analysis Completed Successfully\n")
